3193206be5c11eb7f06b540ad0bcfbccc88ac836
Tasnim Hossain Tanha <htasnim789@gmail.com>
2026-01-21 09:40:43 +0600

    pre-pull commit: UI and font changes

--- lib/screens/recipe_detail_screen.dart
+++ lib/screens/recipe_detail_screen.dart
@@ -9,7 +9,7 @@ class RecipeDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFFEF9E7), // Warm Cream
+      backgroundColor: const Color(0xFFFEF9E7),
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
